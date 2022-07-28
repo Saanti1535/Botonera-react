cec0d226879d00cf58ad2f546de705f784589ad0
Santiago Lopez Roth <slopez@majorkeytech.com>
2022-07-28 13:45:06 -0300

    Uploading app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import { LandingPage } from 'views/LandingPage';
+import { Botonera } from 'views/Botonera';
 
 function App() {
+  const history = createBrowserHistory();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/botonera/:name" element={<Botonera />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
